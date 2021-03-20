782548adc0ba0f7fc6207691b66359c7424a1e0b
Gustavo Gomes <gustsgomesbr@hotmail.com>
2021-03-20 08:35:50 -0400

    now return emojis fully-qualified

--- index.js
+++ index.js
@@ -12,7 +12,28 @@ let emojis = JSON.parse(emojiData);
 console.log('App rodando na porta 4567');
 
 app.get('/', (req, res) => {
-    return res.json(emojis)
+
+        return res.json(emojis);
+
 })
+
+app.get('/fully-qualified', (req, res) =>{
+    let emojiQualified= {};
+    var qualifieds = 0;
+
+        for (let i = 0; i < emojis.length; i++) {
+            if(emojis[i].qualification === 'fully-qualified'){
+                emojiQualified[i] = emojis[i];
+                qualifieds++
+            }
+            
+        }
+        console.log(qualifieds + ', Emojis are Fully Qualified')
+        return res.json(emojiQualified)
+
+})
+
+
+
 
 app.listen('4567')
